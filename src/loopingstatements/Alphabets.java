21ad29f330e41d08e2160ee1cbd52a488756f6ad
adam alamuri <alamuriadam300@gmail.com>
2026-08-06 10:31:41 +0530

    printing alphabets using for loop

--- src/loopingstatements/Alphabets.java
+++ src/loopingstatements/Alphabets.java
@@ -1,4 +1,12 @@
 package loopingstatements;
 
+import java.util.Scanner;
+
 public class Alphabets {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        for(char i = 'Z'; i >= 'A'; i--){
+            System.out.print((char)i + " ");
+        }
+    }
 }
