a11ac39b2fe3c984f0c592af8790841eab3b43d9
adam alamuri <alamuriadam300@gmail.com>
2026-08-10 15:03:47 +0530

    finding sum of natural numbers

--- src/loopingstatements/SumOfNaturalNumbers.java
+++ src/loopingstatements/SumOfNaturalNumbers.java
@@ -2,14 +2,13 @@ package loopingstatements;
 
 import java.util.Scanner;
 
-public class SumOfEvenNumbers {
+public class SumOfNaturalNumbers {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         int sum = 0;
-        for(int i = 0; i <= 100; i++){
-            if(i % 2 == 0){
-                System.out.print(i + " ");
-            }
+        for(int i = 1; i <= 100; i++){
+            sum += i;
+            System.out.print(sum + " ");
         }
     }
 }
