d734e2762c5b10cb6bd957c0dc7d80165311350d
adam alamuri <alamuriadam300@gmail.com>
2026-08-12 11:02:59 +0530

    given number is perfect or not

--- src/loopingstatements/PerfectOrNot.java
+++ src/loopingstatements/PerfectOrNot.java
@@ -3,37 +3,33 @@ package loopingstatements;
 import java.util.Scanner;
 
 public class PerfectOrNot {
-    static Scanner scanner  = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args){
-        PerfectOrNot p = new PerfectOrNot();
-
-        System.out.print("Enter any number : ");
-        int n = scanner.nextInt();
-
-        p.perfectOrNot(n);
-
-        boolean result = p.perfectOrNot(n);
-
-        if(result){
-            System.out.println("Given number is perfect.");
-        }else{
-            System.out.println("Given number is not perfect.");
+    public static boolean isPerfect(int n) {
+        if (n == 0 || n == 1) {
+            return false;
         }
-    }
 
-    public boolean perfectOrNot(int n){
-        boolean flag = false;
         int sum = 0;
 
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0){
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
                 sum += i;
             }
-            if (sum == n){
-                flag = true;
-            }
         }
-        return flag;
+
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        int n = scanner.nextInt();
+
+
+        if (isPerfect(n)) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not Perfect Number");
+        }
     }
 }
