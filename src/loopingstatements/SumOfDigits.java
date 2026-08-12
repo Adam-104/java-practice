50e006334c07d1226ba43de9254834d49538bba2
adam alamuri <alamuriadam300@gmail.com>
2026-08-12 11:04:54 +0530

    sum of given digits

--- src/loopingstatements/SumOfDigits.java
+++ src/loopingstatements/SumOfDigits.java
@@ -1,4 +1,26 @@
 package loopingstatements;
 
+import java.util.Scanner;
+
 public class SumOfDigits {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static int sumOfDigits(int number) {
+        int sum = 0;
+        int rem = 0;
+        while (number > 0) {
+            rem = number % 10;
+            sum += rem;
+            number /= 10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        int number = scanner.nextInt();
+
+        int sum = sumOfDigits(number);
+
+        System.out.println("Sum of digits of given number is " + sum);
+    }
 }
