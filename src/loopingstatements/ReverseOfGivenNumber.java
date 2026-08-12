5cb03421b4298b40299fd03d179851d51d4c9f8f
adam alamuri <alamuriadam300@gmail.com>
2026-08-12 13:03:28 +0530

    reverse of a given number

--- src/loopingstatements/ReverseOfGivenNumber.java
+++ src/loopingstatements/ReverseOfGivenNumber.java
@@ -1,4 +1,28 @@
 package loopingstatements;
 
+import java.util.Scanner;
+
 public class ReverseOfGivenNumber {
+    static Scanner scanner = new Scanner(System.in);
+
+    public int reverseOfGivenNumber(int number) {
+        int rem = 0;
+        int rev = 0;
+        while (number > 0) {
+            rem = number % 10;
+            number = number / 10;
+            rev = rev * 10 + rem;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        int number = scanner.nextInt();
+
+        ReverseOfGivenNumber reverseNumber = new ReverseOfGivenNumber();
+        int result = reverseNumber.reverseOfGivenNumber(number);
+        System.out.println("Reverse of given number is " + result);
+
+    }
 }
