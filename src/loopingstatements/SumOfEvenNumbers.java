d3fa93ec63f3e78aabca85cb386e23bb9f8095a6
adam alamuri <alamuriadam300@gmail.com>
2026-08-12 13:03:54 +0530

    sum of even numbers in specific range

--- src/loopingstatements/SumOfEvenNumbers.java
+++ src/loopingstatements/SumOfEvenNumbers.java
@@ -1,4 +1,25 @@
 package loopingstatements;
 
-public class SumofEvenNumbers {
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void sumOfEvenNumbers(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        System.out.print("enter starting number : ");
+        int start = scanner.nextInt();
+        System.out.print("enter ending number : ");
+        int end = scanner.nextInt();
+
+        sumOfEvenNumbers(start, end);
+    }
 }
