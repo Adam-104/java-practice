4e7d9d11f9f687cef8f46ac0337c024bc4d78ecb
adam alamuri <alamuriadam300@gmail.com>
2026-08-12 11:03:55 +0530

    prime numbers upto specified range

--- src/loopingstatements/PrimeNumbers.java
+++ src/loopingstatements/PrimeNumbers.java
@@ -1,4 +1,25 @@
 package loopingstatements;
 
+import java.util.Scanner;
+
 public class PrimeNumbers {
+    static Scanner scanner = new Scanner(System.in);
+
+    public void primeNumbers(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter start number : ");
+        int start = scanner.nextInt();
+        System.out.print("Enter end number : ");
+        int end = scanner.nextInt();
+
+        PrimeNumbers p = new PrimeNumbers();
+        p.primeNumbers(start, end);
+    }
 }
