6f4454539dc589a072a98aaf864755bc2d687471
adam alamuri <alamuriadam300@gmail.com>
2026-08-14 11:12:05 +0530

    prime numbers

--- src/loopingstatements/PrimeNumbers.java
+++ src/loopingstatements/PrimeNumbers.java
@@ -6,16 +6,29 @@ public class PrimeNumbers {
     static Scanner scanner = new Scanner(System.in);
 
     public void primeNumbers(int start, int end) {
+
         for (int i = start; i <= end; i++) {
-            if (i % 2 != 0) {
+
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
                 System.out.print(i + " ");
             }
         }
     }
 
     public static void main(String[] args) {
+
         System.out.print("Enter start number : ");
         int start = scanner.nextInt();
+
         System.out.print("Enter end number : ");
         int end = scanner.nextInt();
 
