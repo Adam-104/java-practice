d73335d68fc4119f397abeb7eda27536176b2a15
adam alamuri <alamuriadam300@gmail.com>
2026-08-21 13:07:06 +0530

    finding missing elements

--- src/arrays/MissingElements.java
+++ src/arrays/MissingElements.java
@@ -1,15 +1,30 @@
 package arrays;
 
-public class MissingMultipleElements {
+public class MissingElements {
     public static void main(String[] args) {
-        MissingMultipleElements obj = new MissingMultipleElements();
+        MissingElements obj = new MissingElements();
         obj.CommonApproach();
         obj.singleArrayApproach();
         obj.twoArraysApproach();
     }
 
+    //this approach is not used for if there are any duplicate array elements
+    public void CommonApproach() {
+        int[] arr = {1, 3, 5, 6};
+        int n = arr.length + 1;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            actualSum += arr[i];
+        }
+        int missing = expectedSum - actualSum;
+        System.out.println("Missing element : " + missing);
+    }
+
+    //using two arrays time complexity : O(n) and space complexity O(n)
+    //this approach is not used for unsorted array elements
     public void singleArrayApproach() {
-        int[] arr = {1, 2, 4, 6};
+        int[] arr = {1, 3, 5, 6};
         int expected = 1;
         for (int i = 1; i < arr.length; i++) {
             while (expected < arr[i]) {
@@ -21,8 +36,9 @@ public class MissingMultipleElements {
     }
 
     //using two arrays time complexity : O(n) and space complexity O(n)
+    //this approach is best approach it is used for unsorted array and also for duplicates also
     public void twoArraysApproach() {
-        int[] arr = {1, 2, 4, 6};
+        int[] arr = {3, 3, 2, 5};
         int n = 6;
         boolean[] present = new boolean[n + 1];
         for (int i = 0; i < arr.length; i++) {
@@ -30,7 +46,7 @@ public class MissingMultipleElements {
         }
 
         System.out.println("Missing elements : ");
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             if (!present[i]) {
                 System.out.println("Missing element : " + i);
             }
