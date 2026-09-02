599a7eabb3f5077b3440738321271e5e229f90c5
adam alamuri <alamuriadam300@gmail.com>
2026-09-02 17:25:03 +0530

    inheritance demo

--- src/oops/inheritance/example2/InheritanceDemo2.java
+++ src/oops/inheritance/example2/InheritanceDemo2.java
@@ -1,16 +1,12 @@
 package oops.inheritance.example2;
 
 class Person{
-
     String name;
     int age;
-
 }
 
 class Student extends Person{
-
     String collegeName;
-
     public void studentInfo(){
         System.out.println("Student Info");
         System.out.println("Student Name : "         + name);
@@ -18,18 +14,14 @@ class Student extends Person{
         System.out.println("Student College Name : " + collegeName);
         System.out.println("--------------------------------------");
     }
-
 }
 
 public class InheritanceDemo2 {
-
     public static void main(String[] args) {
-
         Student student = new Student();
         student.name = "Adam";
         student.age = 21;
         student.collegeName = "V CUBE";
         student.studentInfo();
     }
-
 }
