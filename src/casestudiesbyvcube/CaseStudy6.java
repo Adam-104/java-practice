7c586016bf158eb7bb14dd54e383cb418f4d8587
adam alamuri <alamuriadam300@gmail.com>
2026-09-08 12:37:47 +0530

    case study 6

---
+++ src/casestudiesbyvcube/CaseStudy6.java
@@ -0,0 +1,44 @@
+//A Hospital wants to develop a system to manage different employee roles such as Doctor, Nurse, and
+//Receptionist using OOP concepts.
+//Create a parent class Employee with a method work() that displays "Working".
+//Create child classes Doctor, Nurse, and Receptionist that override the work() method with
+// their respective responsibilities
+//Int the main() method, use a single employee reference to create objects of each child class and call the work()
+//method
+
+package casestudiesbyvcube;
+
+class Employee{
+    public void work(){
+        System.out.println("Employee is working.");
+    }
+}
+class Doctor extends Employee{
+    @Override
+    public void work(){
+        System.out.println("Doctor is treating patients.");
+    }
+}
+class Nurse extends Employee{
+    @Override
+    public void work(){
+        System.out.println("Nurse taking care of patients.");
+    }
+}
+class Receptionist extends Employee{
+    @Override
+    public void work(){
+        System.out.println("Receptionist talking with patients and make their bills.");
+    }
+}
+
+public class CaseStudy6 {
+    public static void main(String[] args) {
+        Employee e1 = new Doctor();
+        Employee e2 = new Nurse();
+        Employee e3 = new Receptionist();
+        e1.work();
+        e2.work();
+        e3.work();
+    }
+}
